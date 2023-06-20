3e76221157d47b9e4e42d254edeee4ac30c82fc7
Jafar Oday <96765403+J0y9@users.noreply.github.com>
2023-06-20 11:49:25 +0300

    Update Program.cs

--- task5/Jafar/Program.cs
+++ task5/Jafar/Program.cs
@@ -1,20 +1,4 @@
 ﻿using ConsoleApp2;
-
-//LinkedList l = new LinkedList();
-//l.AddFirst("Python");
-//l.AddFirst("JS");
-//l.AddFirst("C#");
-//l.PrintLinkedList();
-
-//Q17
-//Console.WriteLine(l.GetIndex("Python"));
-//Q18
-//Console.WriteLine(l.IsExist("C#"));
-//
-
-//CircularLinkedList cll = new CircularLinkedList();
-//cll.Q4();
-
 void IsContainCSharp(string s)
 {
     if (s.ToLower().Contains("c#"))
