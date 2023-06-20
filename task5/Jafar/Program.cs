f0516a49995174d4a8034f35389ac5e883031028
Jafar Oday <96765403+J0y9@users.noreply.github.com>
2023-06-20 11:49:00 +0300

    Q9 Solution

---
+++ task5/Jafar/Program.cs
@@ -0,0 +1,31 @@
+﻿using ConsoleApp2;
+
+//LinkedList l = new LinkedList();
+//l.AddFirst("Python");
+//l.AddFirst("JS");
+//l.AddFirst("C#");
+//l.PrintLinkedList();
+
+//Q17
+//Console.WriteLine(l.GetIndex("Python"));
+//Q18
+//Console.WriteLine(l.IsExist("C#"));
+//
+
+//CircularLinkedList cll = new CircularLinkedList();
+//cll.Q4();
+
+void IsContainCSharp(string s)
+{
+    if (s.ToLower().Contains("c#"))
+    {
+        Console.WriteLine("C# document found.");
+    }
+    else
+    {
+        Console.WriteLine("Sorry no C# document!");
+    }
+}
+IsContainCSharp("C# Exercises");
+IsContainCSharp("Python Exercises");
+IsContainCSharp("Tutorial on c#");
