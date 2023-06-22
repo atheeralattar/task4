2cedac3d145b7c1b76cd571d180ba4dfd2fdc3f2
Hadi Mohammed <hadi.m.agha@gmail.com>
2023-06-22 17:44:41 +0300

    Hadi Sol Commit

---
+++ task5/Hadi/Task5hadi.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+    public bool MatchThePattern(string string1, string string2)
+    {
+        return ((string1.Substring(0, 1) == string1.Substring(1, 1)) && (string1.Substring(2, 1) == string1.Substring(3)) && (string2.Substring(0, 1) == string2.Substring(1, 1)) && (string2.Substring(2, 1) == string2.Substring(3)));
+    }
+            Console.WriteLine("( AABB ) , ( PPRR ) :>" +"\t" + MatchThePattern("AABB", "PPRR"));
+            Console.WriteLine("( FFGG ) , ( ADAD ) :>" +"\t" + MatchThePattern("FFGG", "ADAD"));
+            Console.WriteLine(Cs("FFGG", "ADAD"));
+            Console.ReadLine();
+}
