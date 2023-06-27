b1f46c2d91dc9bea0f61cc8d90a09769107abd1e
Maytham-altaan <120451567+Maytham-altaan@users.noreply.github.com>
2023-06-27 14:54:17 +0300

    Task 7

--- task7/Maytham/program.cs
+++ task7/Maytham/program.cs
@@ -1,8 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 
-public class Class1
+class Program
 {
-	public Class1()
-	{
-	}
+    static void Main()
+    {
+        Stack<int> s = new Stack<int>();
+
+        s.Push(1);
+        s.Push(2);
+        s.Push(3);
+        s.Push(4);
+        s.Push(5);
+        s.Push(6);
+        s.Push(7);
+        s.Push(8);
+        s.Push(9);
+        s.Push(10);
+
+        int maxNumb = FindMaxNumb(s);
+        Console.WriteLine("The maximum number is " + maxNumb);
+    }
+
+    static int FindMaxNumb(Stack<int> s)
+    {
+        if (s.Count == 0) throw new InvalidOperationException("Stack is empty");
+
+        int maxNumb = Int32.MinValue;
+
+        foreach (int item in s)
+        {
+            if (item > maxNumb)
+            {
+                maxNumb = item;
+            }
+        }
+
+        return maxNumb;
+    }
 }
