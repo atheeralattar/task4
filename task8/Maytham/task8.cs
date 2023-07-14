5c27d5511f2e94c7f8f8dbe958e057be7620c58f
Maytham-altaan <120451567+Maytham-altaan@users.noreply.github.com>
2023-07-14 14:44:29 +0300

    Task8

---
+++ task8/Maytham/task8.cs
@@ -0,0 +1,89 @@
+using System;
+
+public class Node<T>
+{
+    public T Data { get; set; }
+    public Node<T> Next { get; set; }
+
+    public Node(T data)
+    {
+        this.Data = data;
+        this.Next = null;
+    }
+}
+
+public class Queue<T>
+{
+    private Node<T> front;
+    private Node<T> rear;
+
+    public Queue()
+    {
+        this.front = this.rear = null;
+    }
+
+    public void Enqueue(T item)
+    {
+        Node<T> newNode = new Node<T>(item);
+
+        if (this.rear == null)
+        {
+            this.front = this.rear = newNode;
+            return;
+        }
+
+        this.rear.Next = newNode;
+        this.rear = newNode;
+    }
+
+    public void Dequeue()
+    {
+        if (this.front == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return;
+        }
+
+        Node<T> temp = this.front;
+        this.front = this.front.Next;
+
+        if (this.front == null)
+        {
+            this.rear = null;
+        }
+
+        Console.WriteLine("Removed item: " + temp.Data);
+    }
+
+    public void PrintQueue()
+    {
+        Node<T> temp = this.front;
+        while (temp != null)
+        {
+            Console.Write(temp.Data + " ");
+            temp = temp.Next;
+        }
+        Console.WriteLine();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> q = new Queue<int>();
+        q.Enqueue(1);
+        q.Enqueue(2);
+        q.Enqueue(3);
+        Console.WriteLine("Queue elements are: ");
+        q.PrintQueue();
+
+        q.Dequeue();
+        Console.WriteLine("Queue elements are: ");
+        q.PrintQueue();
+
+        q.Enqueue(4);
+        Console.WriteLine("Queue elements are: ");
+        q.PrintQueue();
+    }
+}
